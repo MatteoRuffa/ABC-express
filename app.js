0872f96f46e8c2d397f471c67f383ce0ba43006c
Matteo Ruffa <cortese_17@protonmail.com>
2025-01-14 19:28:59 +0100

    learn CRUD

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/home', (req, res) => {
-    res.send('Hello World!');
+let reviews = [
+    { id: 1, nome: 'Mario', messaggio: 'Ottimo servizio!', valutazione: 5 },
+    { id: 2, nome: 'Anna', messaggio: 'Esperienza nella media.', valutazione: 3 }
+  ];
+
+//try get reviews
+  app.get('/reviews', (req, res) => {
+    res.json(reviews);
+  });
+
+//try get single review
+app.get('/reviews/:id', (req, res) => {
+    const review = reviews.find(r => r.id === parseInt(req.params.id));
+    if (!review) return res.status(404).send('Review not found');
+    res.json(review);
 });
 
-app.post('/contacts', (req, res) => {{
-    const { name, message} = req.body
-    res.send(`Name: ${name}, Message: ${message}`);
-}});
+//POST add a newreview
+app.post('/reviews', (req, res) => {
+    const newReview = {
+        id: reviews.length + 1,
+        nome: req.body.nome,
+        messaggio: req.body.messaggio,
+        valutazione: req.body.valutazione
+    };
+    reviews.push(newReview);
+    res.status(201).json(newReview);
+});
 
+//PUT update a review
+app.put('/reviews/:id', (req, res) => {
+    const review = reviews.find(r => r.id === parseInt(req.params.id));
+    if (!review) return res.status(404).send('Review not found');
+
+    review.nome = req.body.nome;
+    review.messaggio = req.body.messaggio;
+    review.valutazione = req.body.valutazione;
+
+    res.json(review); 
+});
+
+//DELETE delete a review
+app.delete('/review/:is', (req, res ) => {
+    const reviewIndex = reviews.findIndex(r => r.id === parseInt(req.params.id));
+    if (reviewIndex === -1) return res.status(404).send('Review not found');
+
+    reviews.splice(reviewIndex, 1);
+    res.send('Review deleted');
+});
+
+//avvio server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-
+}); 
